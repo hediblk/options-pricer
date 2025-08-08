d360468f394a36341af6615de24a6fb6a5043fe4
Hedi Belkahia <hedibelkahia03@gmail.com>
2025-08-08 12:24:13 +0100

    implement option Greeks

--- black_scholes.py
+++ black_scholes.py
@@ -20,7 +20,6 @@ class BlackScholesPricer:
         # to add later: self.delta, self.gamma, self.vega, self.theta, self.rho
 
     def compute_price(self):
-
         if self.T <= 0 or self.sigma <= 0:
             if self.call:
                 return max(0.0, self.S - self.K)
@@ -34,5 +33,63 @@ class BlackScholesPricer:
             return self.S * norm.cdf(d1) - self.K * math.exp(-self.r * self.T) * norm.cdf(d2)
         else:
             return self.K * math.exp(-self.r * self.T) * norm.cdf(-d2) - self.S * norm.cdf(-d1)
+        
+    def compute_delta(self):
+        if self.T <= 0 or self.sigma <= 0:
+            if self.call:
+                return 1.0 if self.S > self.K else 0.0
+            else:
+                return -1.0 if self.S < self.K else 0.0
 
+        d1 = (math.log(self.S / self.K) + (self.r + 0.5 * self.sigma ** 2) * self.T) / (self.sigma * math.sqrt(self.T))
 
+        if self.call:
+            return norm.cdf(d1)
+        else:
+            return norm.cdf(d1) - 1
+
+    def compute_gamma(self):
+        if self.T <= 0 or self.sigma <= 0:
+            return 0.0
+
+        d1 = (math.log(self.S / self.K) + (self.r + 0.5 * self.sigma ** 2) * self.T) / (self.sigma * math.sqrt(self.T))
+        return norm.pdf(d1) / (self.S * self.sigma * math.sqrt(self.T))
+
+    def compute_vega(self):
+        if self.T <= 0 or self.sigma <= 0:
+            return 0.0
+
+        d1 = (math.log(self.S / self.K) + (self.r + 0.5 * self.sigma ** 2) * self.T) / (self.sigma * math.sqrt(self.T))
+        return self.S * norm.pdf(d1) * math.sqrt(self.T)
+
+    def compute_theta(self):
+        if self.T <= 0 or self.sigma <= 0:
+            if self.call:
+                return -self.K * math.exp(-self.r * self.T) if self.S > self.K else 0.0
+            else:
+                return self.K * math.exp(-self.r * self.T) if self.S < self.K else 0.0
+
+        d1 = (math.log(self.S / self.K) + (self.r + 0.5 * self.sigma ** 2) * self.T) / (self.sigma * math.sqrt(self.T))
+        d2 = d1 - self.sigma * math.sqrt(self.T)
+
+        if self.call:
+            return (-self.S * norm.pdf(d1) * self.sigma / (2 * math.sqrt(self.T)) -
+                    self.r * self.K * math.exp(-self.r * self.T) * norm.cdf(d2))
+        else:
+            return (-self.S * norm.pdf(d1) * self.sigma / (2 * math.sqrt(self.T)) +
+                    self.r * self.K * math.exp(-self.r * self.T) * norm.cdf(-d2))
+
+    def compute_rho(self):
+        if self.T <= 0 or self.sigma <= 0:
+            if self.call:
+                return self.K * self.T * math.exp(-self.r * self.T) if self.S > self.K else 0.0
+            else:
+                return -self.K * self.T * math.exp(-self.r * self.T) if self.S < self.K else 0.0
+
+        d2 = (math.log(self.S / self.K) + (self.r - 0.5 * self.sigma ** 2) * self.T) / (self.sigma * math.sqrt(self.T))
+        if self.call:
+            return self.K * self.T * math.exp(-self.r * self.T) * norm.cdf(d2)
+        else:
+            return -self.K * self.T * math.exp(-self.r * self.T) * norm.cdf(-d2)
+        
+    
